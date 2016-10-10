c247782735c29eb2f3aa6ebfe90c60e3d873ef06
Aleksandar Kamenov <AKamenov@infragistics.com>
2016-10-10 14:35:46 +0300

    add login register layouts without functionality

--- MovieHub/app/src/main/java/info/noname/moviehub/LoginManager.java
+++ MovieHub/app/src/main/java/info/noname/moviehub/LoginManager.java
@@ -1,25 +1,53 @@
 package info.noname.moviehub;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by AKamenov on 10/9/2016.
  */
 
-public class LoginManager {
-    // With this instance we are going to save data to the local sotrage
-    private SharedPreferences _preferences;
+public class LoginManager extends AppCompatActivity implements View.OnClickListener{
+//    // With this instance we are going to save data to the local sotrage
+//    private SharedPreferences _preferences;
+//
+//    // With this we gonna commit each new action to the editor
+//    private SharedPreferences.Editor _preferenceEditor;
+//
+//    private Context _context;
+//    int PRIVATE_MODE = 0;
+//
+//    // Is-logged KEY
+//    private static final String LOGIN_KEY = "login";
+//
+//    // prefs name -> With thi we are going to load out xml preference file from system
+//    private static final String prefsName = "MovieHub";
 
-    // With this we gonna commit each new action to the editor
-    private SharedPreferences.Editor _preferenceEditor;
+    private Button mLoginButton;
+    private EditText mUsernameEditText;
+    private EditText mPasswrodEditText;
 
-    private Context _context;
-    int PRIVATE_MODE = 0;
 
-    // Is-logged KEY
-    private static final String LOGIN_KEY = "login";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
 
-    // prefs name -> With thi we are going to load out xml preference file from system
-    private static final String prefsName = "MovieHub";
+        mLoginButton = (Button) findViewById(R.id.login_button);
+        mUsernameEditText = (EditText) findViewById(R.id.username_edit_text);
+        mPasswrodEditText = (EditText) findViewById(R.id.password_edit_text);
+
+        mLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.login_button) {
+
+        }
+    }
 }
