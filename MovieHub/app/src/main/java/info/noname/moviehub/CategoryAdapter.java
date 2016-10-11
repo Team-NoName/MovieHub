7b1dca571eed04b978d123eba4130c69f6017fc1
zhosti <zhostovivan@gmail.com>
2016-10-12 00:42:36 +0300

    Add categories recycle view

---
+++ MovieHub/app/src/main/java/info/noname/moviehub/CategoryAdapter.java
@@ -0,0 +1,71 @@
+package info.noname.moviehub;
+
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import info.noname.moviehub.models.Category;
+
+/**
+ * Created by Ivan on 10/11/2016.
+ */
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+    private List<Category> mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextNameView;
+        public RelativeLayout parent;
+
+        public ViewHolder(View v) {
+            super(v);
+
+            mTextNameView = (TextView) v.findViewById(R.id.categoryName);
+            parent = (RelativeLayout) v.findViewById(R.id.parent);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public CategoryAdapter(List<Category> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public CategoryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.category_layout, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+      //  holder.mTextNameView.setText(mDataset[position]);
+        holder.mTextNameView.setText(mDataset.get(position).get_name());
+        holder.parent.setBackgroundColor(Color.parseColor("#3333ff"));
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
+
--- MovieHub/app/src/main/java/info/noname/moviehub/MainActivity.java
+++ MovieHub/app/src/main/java/info/noname/moviehub/MainActivity.java
@@ -4,14 +4,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.List;
+
+import info.noname.moviehub.models.Category;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public final Context mCtx = this;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     private Button mBtnLogin;
 
@@ -32,6 +42,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setSupportActionBar(toolbar);
         this.setTitle(getString(R.string.menu));
 
+        mRecyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
+
+        // mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        List<Category> myDataset = Category.listAll(Category.class);
+
+        // specify an adapter (see also next example)
+        mAdapter = new CategoryAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
         mDialogManager = new AlertDialogManager();
 
         mLoginButton = (Button) findViewById(R.id.login_button);
@@ -41,8 +65,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mRegisterLink = (TextView) findViewById(R.id.register_link);
 
-        mLoginButton.setOnClickListener(this);
-        mRegisterLink.setOnClickListener(this);
+        //comment just for test categories
+
+        //mLoginButton.setOnClickListener(this);
+        //mRegisterLink.setOnClickListener(this);
     }
 
     @Override
