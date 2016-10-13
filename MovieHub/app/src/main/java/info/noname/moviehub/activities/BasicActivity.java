4fece29388404e5a90cc14322165ebc5fa8a6093
Aleksandar Kamenov <AKamenov@infragistics.com>
2016-10-14 00:47:40 +0300

    login register and logout are already implemented

--- MovieHub/app/src/main/java/info/noname/moviehub/activities/BasicActivity.java
+++ MovieHub/app/src/main/java/info/noname/moviehub/activities/BasicActivity.java
@@ -1,15 +1,18 @@
 package info.noname.moviehub.activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
+import info.noname.moviehub.AlertDialogManager;
 import info.noname.moviehub.R;
+import info.noname.moviehub.UserLocalStore;
+import info.noname.moviehub.fragments.LoginFragmentManager;
 
 /**
  * Created by AKamenov on 10/13/2016.
@@ -19,6 +22,12 @@ public class BasicActivity extends AppCompatActivity {
 
     private final Context mCtx = this;
 
+    public UserLocalStore userLocalStore;
+
+    public AlertDialogManager dialogManager;
+
+    private LoginFragmentManager mLoginManager = new LoginFragmentManager();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +36,9 @@ public class BasicActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         this.setTitle(getString(R.string.menu));
+
+        this.userLocalStore = new UserLocalStore(this);
+        this.dialogManager = new AlertDialogManager();
     }
 
     @Override
@@ -40,8 +52,13 @@ public class BasicActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Toast.makeText(this.mCtx, "Settings", Toast.LENGTH_LONG).show();
+        if (id == R.id.action_logout) {
+            this.userLocalStore.setUserLoggedIn(false);
+            this.userLocalStore.clearUserData();
+
+            Intent intent = new Intent(BasicActivity.this, MainActivity.class);
+
+            startActivity(intent);
             return true;
         }
 
--- MovieHub/app/src/main/java/info/noname/moviehub/activities/MainActivity.java
+++ MovieHub/app/src/main/java/info/noname/moviehub/activities/MainActivity.java
@@ -2,23 +2,15 @@ package info.noname.moviehub.activities;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import info.noname.moviehub.AlertDialogManager;
 import info.noname.moviehub.R;
-import info.noname.moviehub.UserLocalStore;
 import info.noname.moviehub.fragments.LoginFragmentManager;
 import info.noname.moviehub.models.User;
 
-public class MainActivity extends BasicActivity implements View.OnClickListener{
+public class MainActivity extends BasicActivity {
     public final Context mCtx = this;
 
-    private AlertDialogManager mDialogManager;
 
-    private Button mGoToLogin;
-
-    private UserLocalStore mUserLocalStore;
 
     private LoginFragmentManager mLoginManager = new LoginFragmentManager();
 
@@ -28,6 +20,7 @@ public class MainActivity extends BasicActivity implements View.OnClickListener{
 
         User getAuthenticatedUser = this.authenticate();
         if (getAuthenticatedUser == null) {
+
             getFragmentManager().beginTransaction().replace(R.id.fragment_content, this.mLoginManager).commit();
         }
     }
@@ -35,24 +28,17 @@ public class MainActivity extends BasicActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mDialogManager = new AlertDialogManager();
-        mGoToLogin = (Button) findViewById(R.id.logout_button);
-
-        mGoToLogin.setOnClickListener(this);
-
-        this.mUserLocalStore = new UserLocalStore(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.logout_button) {
-            this.mUserLocalStore.clearUserData();
-            this.mUserLocalStore.setUserLoggedIn(false);
-        }
-    }
-
+//    @Override
+//    public void onClick(View v) {
+//        if (v.getId() == R.id.logout_button) {
+//            this.mUserLocalStore.clearUserData();
+//            this.mUserLocalStore.setUserLoggedIn(false);
+//        }
+//    }
+//
     private User authenticate() {
-        return this.mUserLocalStore.getLoggedInUser();
+        return this.userLocalStore.getLoggedInUser();
     }
 }
